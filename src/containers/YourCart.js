47eff7e1268794732f6894603851b5e6ef5461d1
hbj <heyhal19@gmail.com>
2020-07-06 14:12:32 -0700

    readme changes

--- src/containers/YourCart.js
+++ src/containers/YourCart.js
@@ -5,7 +5,7 @@ class YourCart extends Component {
 
   render() {
     return (
-      <div className="ui segment inverted olive bot-army">
+      <div className="ui segment inverted yellow bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
             {/*...and here...*/}
